5716eecf3e73d1f8ff492e550a59308c568d7886
James Stephaniuk <jstephaniuk@gmail.com>
2015-10-27 15:03:07 -0700

    Dataset name support

--- Python/RepairBrokenLayers.py
+++ Python/RepairBrokenLayers.py
@@ -1,15 +1,17 @@
 import arcpy
+import os
 
 mxd = arcpy.mapping.MapDocument('CURRENT')
 
-# lyr = arcpy.mapping.ListLayers(mxd)[0]
 lyrLst = arcpy.mapping.ListLayers(mxd)
-replacePath = r"P:\15030_32_EBC_Digital_Mapping\MapData\2015 By Election Data\replicateSDE_Logfiles"
+
+# Change to location of your geodatabase.
+gdbPath = r"P:\15030_32_EBC_Digital_Mapping\MapData\2015 By Election Data\Replicate.gdb"
 
 for lyr in lyrLst:
-    if lyr.supports("WORKSPACEPATH"):
-        if lyr.isBroken:
-            mxd.findAndReplaceWorkspacePaths(lyr.workspacePath, replacePath, False)
+    if lyr.isBroken:
+        if lyr.supports("DATASETNAME"):
+            mxd.findAndReplaceWorkspacePaths(lyr.workspacePath, gdbPath + lyr.datasetName, False)
             print "Layer {} repaired.".format(lyr.name)
 
 arcpy.RefreshTOC()
