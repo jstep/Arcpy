b83e0aeee720dec279e89018376d7389abb8ae48
James Stephaniuk <jstephaniuk@gmail.com>
2015-10-27 13:45:17 -0700

    Optimized broken layer repair

--- Python/RepairBrokenLayers.py
+++ Python/RepairBrokenLayers.py
@@ -4,11 +4,12 @@ mxd = arcpy.mapping.MapDocument('CURRENT')
 
 # lyr = arcpy.mapping.ListLayers(mxd)[0]
 lyrLst = arcpy.mapping.ListLayers(mxd)
+replacePath = r"P:\15030_32_EBC_Digital_Mapping\MapData\2015 By Election Data\replicateSDE_Logfiles"
 
 for lyr in lyrLst:
     if lyr.supports("WORKSPACEPATH"):
         if lyr.isBroken:
-            mxd.findAndReplaceWorkspacePaths(lyr.workspacePath, r"C:\Users\jastepha\Desktop\testOutput\cat\bar\baz", True)
+            mxd.findAndReplaceWorkspacePaths(lyr.workspacePath, replacePath, False)
             print "Layer {} repaired.".format(lyr.name)
 
 arcpy.RefreshTOC()
