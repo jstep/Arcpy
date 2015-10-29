76c4129846890b4b0de0d11f7a5160f8bbe18f6e
James Stephaniuk <jstephaniuk@gmail.com>
2015-10-29 10:25:22 -0700

    Restructure for top left handle on elements. Better column width dection

--- svaBoxSizing.py
+++ svaBoxSizing.py
@@ -7,17 +7,23 @@ import arcpy
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
 # Feature class name.
-mapLyr = arcpy.mapping.ListLayers(mxd,"SVAs_Internal_LabelsAnno*")[0]
+mapLyr = arcpy.mapping.ListLayers(mxd,"PUT_NAME_OF_LAYER_WITH_SVA_INFO_HERE")[0]
 
-# SVA container (graphic box) name.
+# SVA container (graphic box) name. Match this name to the SVA Box element name in ArcMap.
 svaBox = "svaBox"
 
 # Graphic box layout settings.
-# fontSize = 10.0
-padding = 0.2
-spacer = 0.1
+padding = 0.2 # Distance (in inches) around edge of box to inner elements.
+spacer = 0.1  # Distance (in inches) between inner elements.
 
-# SVA text element names.
+# SVA source feature class field names. Add the field names from the layer that you want to populate the SVA Table.
+svaTxtField  = "FIELD_NAME"
+facilityName = "FIELD_NAME"
+addrNo       = "FIELD_NAME"
+addrName     = "FIELD_NAME"
+city         = "FIELD_NAME"
+
+# SVA text element names. Match these names to the element names in ArcMap.
 SVATitleText  = "SVATitle"
 svaTitleElem0 = "SVA_TitleElem0"
 svaTitleElem1 = "SVA_TitleElem1"
@@ -27,36 +33,31 @@ svaTxtElem0   = "SVA_textElem0"
 svaTxtElem1   = "SVA_textElem1"
 svaTxtElem2   = "SVA_textElem2"
 svaTxtElem3   = "SVA_textElem3"
-# SVA feature class field names.
-svaTxtField0 = "textstring"
-svaTxtField1 = "textstring"
-svaTxtField2 = "textstring"
-svaTxtField3 = "shape_length"
 
-SVATitle   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", SVATitleText)[0]
-titleElem0 = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem0)[0]
-titleElem1 = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem1)[0]
-titleElem2 = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem2)[0]
-titleElem3 = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem3)[0]
-svaElem    = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem0)[0]
-addrElem   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem1)[0]
-txtElem2   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem2)[0]
-txtElem3   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem3)[0]
+mainTitle     = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", SVATitleText)[0]
+svaTitle      = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem0)[0]
+facTitle      = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem1)[0]
+addrTitle     = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem2)[0]
+cityTitle     = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTitleElem3)[0]
+svaTxtElem    = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem0)[0]
+facTxtElem    = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem1)[0]
+addrTxtElem   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem2)[0]
+cityTxtElem   = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", svaTxtElem3)[0]
 
-elemLst = [svaElem,addrElem, txtElem2, txtElem3]
+elemLst = [svaTxtElem, facTxtElem, addrTxtElem, cityTxtElem]
 
-# Captions cannot be empty therefore a single char (space). Set all font sizes to same.
+# Captions cannot be empty therefore a single char. Set all font sizes to same.
 for elem in elemLst:
 	elem.text = " "
-	#elem.fontSize = fontSize ### TODO
 
-# Create columns.
+# Populate columns.
 rows = arcpy.SearchCursor(mapLyr.dataSource)
 for row in rows:
-	svaElem.text  += "{}\n".format(row.getValue(svaTxtField0))
-	addrElem.text += "{}\n".format(row.getValue(svaTxtField1))
-	txtElem2.text += "{}\n".format(row.getValue(svaTxtField2))
-	txtElem3.text += "{}\n".format(row.getValue(svaTxtField3))
+	svaTxtElem.text  += "{}\n".format(row.getValue(svaTxtField))
+	facTxtElem.text  += "{}\n".format(row.getValue(facilityName))
+	addrTxtElem.text += "{} ".format(row.getValue(addrNo))
+	addrTxtElem.text += "{}\n".format(row.getValue(addrName))
+	cityTxtElem.text += "{}\n".format(row.getValue(city))
 
 # Slice off leading space char.
 for elem in elemLst:
@@ -64,36 +65,39 @@ for elem in elemLst:
 
 svaBox = arcpy.mapping.ListLayoutElements(mxd, "GRAPHIC_ELEMENT", svaBox)[0]
 
-# Position text elements.
-addrElem.elementPositionY = txtElem2.elementPositionY = txtElem3.elementPositionY = svaElem.elementPositionY = svaBox.elementPositionY + padding
-svaElem.elementPositionX  = svaBox.elementPositionX   + padding
-addrElem.elementPositionX = svaElem.elementPositionX  + svaElem.elementWidth + spacer
-txtElem2.elementPositionX = addrElem.elementPositionX + addrElem.elementWidth + spacer
-txtElem3.elementPositionX = txtElem2.elementPositionX + txtElem2.elementWidth + spacer
+# Position main title elem.
+mainTitle.elementPositionX = svaBox.elementPositionX + padding
+mainTitle.elementPositionY = svaBox.elementPositionY - padding
 
-titleElem0.elementPositionX = svaElem.elementPositionX
-titleElem1.elementPositionX = addrElem.elementPositionX
-titleElem2.elementPositionX = txtElem2.elementPositionX
-titleElem3.elementPositionX = txtElem3.elementPositionX
-
-titleElem0.elementPositionY = svaElem.elementPositionY + svaElem.elementHeight + spacer
-titleElem1.elementPositionY = addrElem.elementPositionY + addrElem.elementHeight + spacer
-titleElem2.elementPositionY = txtElem2.elementPositionY + txtElem2.elementHeight + spacer
-titleElem3.elementPositionY = txtElem3.elementPositionY + txtElem3.elementHeight + spacer
-
-SVATitle.elementPositionX = titleElem0.elementPositionX
-SVATitle.elementPositionY = titleElem0.elementPositionY + titleElem0.elementHeight + spacer
+# Position the titles and text elements relative to main title and each other.
+# Col 1
+widthCol1 = max(svaTitle.elementWidth, svaTxtElem.elementWidth) 
+svaTitle.elementPositionX = svaTxtElem.elementPositionX = mainTitle.elementPositionX
+svaTitle.elementPositionY = mainTitle.elementPositionY - svaTitle.elementHeight - padding # Uses padding instead of spacer to give main title more room.
+svaTxtElem.elementPositionY = svaTitle.elementPositionY - svaTitle.elementHeight
+# Col 2
+widthCol2 = max(facTitle.elementWidth, facTxtElem.elementWidth)
+facTitle.elementPositionY = svaTitle.elementPositionY
+facTxtElem.elementPositionY = svaTxtElem.elementPositionY
+facTitle.elementPositionX = svaTitle.elementPositionX + widthCol1 + spacer
+facTxtElem.elementPositionX = svaTxtElem.elementPositionX + widthCol1 + spacer
+# Col 3
+widthCol3 = max(addrTitle.elementWidth, addrTxtElem.elementWidth)
+addrTitle.elementPositionY = facTitle.elementPositionY
+addrTxtElem.elementPositionY = facTxtElem.elementPositionY
+addrTitle.elementPositionX = facTitle.elementPositionX + widthCol2 + spacer
+addrTxtElem.elementPositionX = facTxtElem.elementPositionX + widthCol2 + spacer
+# Col 4
+widthCol4 = max(cityTitle.elementWidth, cityTxtElem.elementWidth)
+cityTitle.elementPositionY = addrTitle.elementPositionY
+cityTxtElem.elementPositionY = addrTxtElem.elementPositionY
+cityTitle.elementPositionX = addrTitle.elementPositionX + widthCol3 + spacer
+cityTxtElem.elementPositionX = addrTxtElem.elementPositionX + widthCol3 + spacer
 
 # Set the svaBox height and width derived from text elements heights and widths.
-tallestElem = max(svaElem.elementHeight, addrElem.elementHeight, txtElem2.elementHeight, txtElem3.elementHeight)
-boxHeight = SVATitle.elementPositionY  - svaElem.elementPositionY + SVATitle.elementHeight
-svaBox.elementHeight = boxHeight + (padding * 2)
-svaBox.elementWidth = txtElem3.elementPositionX - svaElem.elementPositionX + txtElem3.elementWidth + (padding * 2)
-svaBox.elementWidth = max(txtElem3.elementPositionX - svaElem.elementPositionX + txtElem3.elementWidth, SVATitle.elementWidth) + (padding * 2)
-
-# Set the svaBox position derived from svaTxtElem0 XY position.
-svaBox.elementPositionX = svaElem.elementPositionX - padding
-svaBox.elementPositionY = svaElem.elementPositionY - padding
-
-# SVA Box title.
-# svaBox.elementHeight += 0.5
+tallestTxtElem = max(svaTxtElem.elementHeight, facTxtElem.elementHeight, addrTxtElem.elementHeight, cityTxtElem.elementHeight)
+tallestTitleElem = max(svaTitle.elementHeight, facTitle.elementHeight, addrTitle.elementHeight, cityTitle.elementHeight)
+colsWidths = widthCol1 + widthCol2 + widthCol3 + widthCol4
+textWidth = colsWidths + (spacer * 3)
+svaBox.elementHeight = tallestTxtElem + tallestTitleElem + mainTitle.elementHeight + (padding * 2)
+svaBox.elementWidth = max(textWidth, mainTitle.elementWidth) + (padding * 2)
