30a9b990b6b065c69986ecdcca4c7cf6bcdec0b9
unknown <jastepha@EBCOGEO1.electionsbc.local>
2015-11-04 14:08:19 -0800

    Function to build a definition query based on current selection.

---
+++ querySelection.py
@@ -0,0 +1,16 @@
+def querySelection():
+    """Builds a definition query to display only the currently 
+    selected features for that layer.
+    """
+    # import getSelectionSet
+    mxd = arcpy.mapping.MapDocument("CURRENT")
+    ids, name, df, oid = getSelectionSet()
+    lyr = arcpy.mapping.ListLayers(mxd, name, df)[0]
+    
+    # Build SQL clause.
+    whereClause = "{} in ({})".format(oid, ','.join(str(id) for id in ids))
+
+    # Apply the definition query.
+    lyr.definitionQuery = whereClause
+
+    arcpy.RefreshActiveView()
