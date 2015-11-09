845f9fb01ebacd7a62d5de54a39e4425783d3013
jstep <jstephaniuk@gmail.com>
2015-11-09 15:12:23 -0800

    Generalize exporter function. Add PNG class

--- PDF_Export_Addin/Install/PDF_Export_Addin_addin.py
+++ PDF_Export_Addin/Install/PDF_Export_Addin_addin.py
@@ -1,14 +1,14 @@
 import arcpy
-import pythonaddins
 import os
+import pythonaddins
 
-def exporter(colorspace, dpi):
+def exporter(colorspace, dpi, fileType="PDF"):
     try:
         mxd = arcpy.mapping.MapDocument('CURRENT')
         ddp = mxd.dataDrivenPages
         pageName = ddp.pageRow.getValue(ddp.pageNameField.name)
 
-        outDir = pythonaddins.OpenDialog("Path to PDF output", False, os.path.dirname(mxd.filePath), "Select Folder")
+        outDir = pythonaddins.OpenDialog("Path to output directory", False, os.path.dirname(mxd.filePath), "Select Folder")
         if not outDir:
             outDir = os.path.dirname(mxd.filePath)
 
@@ -25,21 +25,20 @@ def exporter(colorspace, dpi):
             orient = "{}x{}".format(str(mxd.pageSize[0]), str(mxd.pageSize[1]))
 
         # Join output directory with formatted file name.
-        PDFOut = os.path.join(outDir,"{}_{}_{}_{}".format(pageName,str(dpi),colorspace,orient))
-        # PDF Export
-        arcpy.mapping.ExportToPDF(mxd, PDFOut, resolution=dpi,image_quality="BEST",colorspace=colorspace, convert_markers=True)
+        PathOut = os.path.join(outDir,"{}_{}_{}_{}".format(pageName,str(dpi),colorspace,orient))
+        # Export
+        if fileType == "PDF":
+            arcpy.mapping.ExportToPDF(mxd, PathOut, resolution=dpi,image_quality="BEST",colorspace=colorspace, convert_markers=True)
+        elif fileType == "PNG":
+            arcpy.mapping.ExportToPNG(mxd, PathOut, resolution=dpi,color_mode=colorspace)
 
-        pythonaddins.MessageBox("PDF saved to \n{}\n as\n {}".format(outDir, os.path.split(PDFOut)[1]),"PDF Save Location")
+        pythonaddins.MessageBox("Saved to \n{}\n as\n {}".format(outDir, os.path.split(PathOut)[1]),"Save Location")
 
         del mxd, ddp, pageName, outDir, orient, dpi, colorspace
     except Exception as e:
         pythonaddins.MessageBox(e, "Error Message")
         print e
 
-
-###############################################################################################################################
-
-# Button Classes.
 
 class CMYK288(object):
     """Implementation for PDF_Export_Addin_addin.button (Button)"""
@@ -50,6 +49,15 @@ class CMYK288(object):
         # Call to exporter function.
         exporter("CMYK", 144)
 
+
+class PNG_Draft(object):
+    """Implementation for PDF_Export_Addin_addin.pngDraft (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        exporter("24-BIT_TRUE_COLOR", 1152, "PNG")
+
 class RGB144(object):
     """Implementation for PDF_Export_Addin_addin.button_1 (Button)"""
     def __init__(self):
@@ -58,4 +66,3 @@ class RGB144(object):
     def onClick(self):
         # Call to exporter function.
         exporter("RGB", 144)
-        
